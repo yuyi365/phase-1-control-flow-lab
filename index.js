576d84620a252de540c9014aa3f37278d186ae1b
Yuyi <yuyi365@gmail.com>
2021-10-12 18:42:47 -0400

    commit tree ef45fd1b17072b931b2e6b2fcfdc83c2bd014eef

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideHeight){
+
+  if (rideHeight > 2500) {
+    return("No can do.");
+  } else if (rideHeight > 2000) {
+    return("I will gladly take your thirty bucks.");
+  } else if (rideHeight <= 400) {
+    return("This one is on me!");
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+
+  return (cityName == "NYC" ? "Ok, sounds good." : "No go.");
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipPercentage){
+  
+  switch (tipPercentage) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
